0c2c6983de5b7f4e8cdb30301ced59db110f5f8b
AhmerAtiq <ahmer.atiq@gmail.com>
2023-04-10 22:29:17 +0400

    Commit on 10042023

---
+++ catalog-service/cli.cmd
@@ -0,0 +1,3 @@
+set JAVA_HOME=D:\java\jdk-17.0.2
+set PATH=%JAVA_HOME%\bin;%PATH%
+start cmd
---
+++ catalog-service/src/main/java/com/polarbookshop/catalogservice/HomeController.java
@@ -0,0 +1,23 @@
+package com.polarbookshop.catalogservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.polarbookshop.catalogservice.config.PolarProperties;
+
+@RestController
+public class HomeController {
+
+	private final PolarProperties polarProperties;
+
+	public HomeController(PolarProperties polarProperties) {
+		super();
+		this.polarProperties = polarProperties;
+	}
+
+	@GetMapping("/")
+	public String getGreeting() {
+		return polarProperties.getGreeting();
+	}
+
+}
